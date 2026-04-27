3ce0d22e7f51d10aa79f5dd597df999b2ff3c19b
Mahashivkarn <dhruvkumarrajput007@gmail.com>
2026-04-27 21:01:43 +0530

    My HTML And CSS Code

--- Apnacollege.c
+++ Apnacollege.c
@@ -1,13 +1,10 @@
 #include<stdio.h>
 int main(){
-   int i=1,j=1;
-   while(i++<=100){
-    while(j++<=200){
-        if(j==150)
-        break;
-        else 
-        printf("%d %d\n",i,j);
-    }
-   }
+   int j;
+   printf("Enter a number:");
+   scanf("%d",&j);
+  for(int i=1;i<=10;i++){
+    printf("%d * %d =%d\n",j, i,i*j) ;
+  }
    return 0;
 }
---
+++ Code.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+void printfhello();
+
+int main(){
+   printfhello();
+    return 0;
+}
+ void printfhello(){
+    printf("Hello!\n");
+ }
