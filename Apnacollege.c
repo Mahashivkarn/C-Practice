433614e1027be016bf5d8398c1d7cc1275fb5ab5
Mahashivkarn <dhruvkumarrajput007@gmail.com>
2026-04-30 11:47:21 +0530

    Leap Year Question

--- Apnacollege.c
+++ Apnacollege.c
@@ -1,32 +1,13 @@
 #include<stdio.h>
+// Checking if the is leap year or not.
 int main(){
-  int l,b,A0,A1,A2,A3,A4,A5,A6,A7,A8;
-  l=1189;
-  b=841;
-  printf("A0 is %d * %d\n",l,b);
-  A1=b;
-  b=1189/2;
-  printf("A1 is %d * %d\n",A1,b);
-  A2=b;
-  b=841/2;
-  printf("A2 is %d * %d\n",A2,b);
-  A3=b;
-  b=594/2;
-  printf("A3 is %d * %d\n",A3,b);
-  A4=b;
-  b=420/2;
-  printf("A4 is %d * %d\n",A4,b);
-  A5=b;
-  b=297/2;
-  printf("A5 is %d * %d\n",A5,b);
-  A6=b;
-  b=210/2;
-  printf("A6 is %d * %d\n",A6,b);
-  A7=b;
-  b=148/2;
-  printf("A7 is %d * %d\n",A7,b);
-  A8=b;
-  b=105/2;
-  printf("A8 is %d * %d\n",A8,b);
+  int year;
+  printf("Enter a Year:\n");
+  scanf("%d",&year);
+  if(year%4==0){
+    printf("Year is a leap Year");
+  } else{
+    printf("Year is not a leap year");
+  }
   return 0;
 }
