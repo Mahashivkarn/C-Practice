60fb5115bbc0ee59ecf526380540c477910c48f7
Mahashivkarn <dhruvkumarrajput007@gmail.com>
2026-05-05 00:31:26 +0530

    Printing Triangles

--- Apnacollegehw.c
+++ Apnacollegehw.c
@@ -1,19 +1,9 @@
 #include<stdio.h>
-// Revresing a 5 digit number
 int main(){
-int n,one,two,three,four,five,reverse;
-printf("Enter a 5 digit number:");
-scanf("%d",&n);
-five=n%10;
-n=n/10;
-four=n%10;
-n=n/10;
-three=n%10;
-n=n/10;
-two=n%10;
-n=n/10;
-one=n;
-reverse=five*10000+four*1000+three*100+two*10+one;
-printf("reverse of the number is :%d\n",reverse);
-return 0;
+    for(int i=1;i<=5;i++){
+        for(int j=1;j<=i;j++){
+            printf("*");
+        }
+        printf("\n");
+    }
 }
