e60ce9fff94e4a894eac9e24ada8084d51bd6d90
Mahashivkarn <dhruvkumarrajput007@gmail.com>
2026-06-03 15:02:59 +0530

    The Matchsticks Game

--- Helloworld.c
+++ Helloworld.c
@@ -1,9 +1,31 @@
 #include<stdio.h>
+// Programming for always computer to win in matchsticks game 
 int main(){
-    int i=4 ,j=-1,k=0,w,x,y,z;
-    w= i|| j|| k;
-    x= i&& j && k;
-    z=i &&j || k;
-    printf("w=%d x =%d y= %d z=%d\n",w,x,y,z);
+    int matchsticks=21;
+    int user,compu;
+    printf("The Matchsticks Game \n");
+
+    while(matchsticks >1){
+        printf("\nMatchsticks left=%d\n",matchsticks);
+
+        printf("Pick 1,2,3 or 4 matchsticks");
+        scanf("%d",&user);
+
+    if(user<1 || user>4){
+        printf("Invalid Choise!\n");
+
+        continue;
+    }
+    compu=5-user;
+
+    matchsticks=matchsticks-user-compu;
+
+    printf("\nComputer picks %d matchsticks\n",compu);
+    }
+    printf("\nOnly one matchsticks left\n");
+
+    printf("\nYou have to pick it\n");
+
+    printf("\nHehehe You Loose!\n");
     return 0;
 }
