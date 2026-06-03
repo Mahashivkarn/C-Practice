dca0bf2d51a258f2779f4e6ea9b6e87b33f11501
Mahashivkarn <dhruvkumarrajput007@gmail.com>
2026-06-03 14:33:38 +0530

    calculating armstrong number between 1 to 500

--- Code.c
+++ Code.c
@@ -1,11 +1,22 @@
-#include<stdio.h>
-int main(){
-   int a,b,c,d;
-   printf("Enter a two number");
-   scanf("%d %d",&a,&b);
-   c=a;
-   a=b;
-   b=c;
-   printf("Number are:%d %d \n",a,b);
+#include <stdio.h>
+// calculating armstrong number between 1 to 500
+int main()
+{
+   int n = 1, i, m, num;
+   while (n < 500)
+   {
+      i = n;
+      num = 0;
+      while (i > 0)
+      {
+         m = i % 10;
+         num = num + m * m * m;
+         i = i / 10;
+      }
+      if(num==n){
+         printf("%d\n",n);
+      }
+      n++;
+   }
    return 0;
 }
