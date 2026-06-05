066d32ed25d0e3faed4920f4b791844950df0dba
Mahashivkarn <dhruvkumarrajput007@gmail.com>
2026-06-05 15:40:54 +0530

    Finding Ramanujan number upto a reasonable limit

--- Code.c
+++ Code.c
@@ -1,19 +1,25 @@
 #include <stdio.h>
+/*Finding Ramanujan number upto a reasonable limit*/
 int main()
 {
- int a,b,c;
- for(a=1;a<=30;a++)
- {
-   for(b=a;b<=30;b++)
+ int a,b,c,d,n,m;
+ for (a=1;a<=50;a++)
+{
+  for(b=a;b<=50;b++)
+  {
+   n=a*a*a + b*b*b;
+   for(c=a+1;c<=50;c++)
    {
-      for(c=b;c<=30;c++)
+      for(d=c;d<=50;d++)
       {
-         if(a*a+b*b==c*c)
+         m=c*c*c + d*d*d;
+         if(n==m)
          {
-          printf("Triplets are: %d %d %d\n",a,b,c);
+            printf("%d = %d^3 + %d^3  = %d^3 + %d^3\n",n,a,b,c,d);
          }
       }
-   }
- }
+    }
+  }
+}
  return 0;
 }
