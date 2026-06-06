74c6f3c302f56ef69590604bec0611d6a2931216
Mahashivkarn <dhruvkumarrajput007@gmail.com>
2026-06-06 10:04:34 +0530

    printing 1 to10 in pyramid form

--- Helloworld.c
+++ Helloworld.c
@@ -1,21 +1,17 @@
 #include<stdio.h>
-#include<math.h>
-//FInding lograthmic value 
 int main(){
-    float n,sum=0,a;
-    printf("Enter a number:\n");
-    scanf("%f",&a);
-for(int i=1;i<=7;i++)
-{
-    n=pow((a-1)/a,i);
-    if(i==1){
-        sum =sum +n;
-    }else
+    int i,j,sum=1;
+    for(i=1;i<=4;i++)
     {
-        sum=sum+n/2;
+        for(j=1;j<=4-i;j++)
+        {
+            printf("   ");
+        } for(j=1;j<=i;j++)
+        {
+            printf("%d     ",sum);
+            sum++;
+        }
+        printf("\n");
     }
-    
-}
-printf("The answer is : %f",sum);
 return 0;
 }
