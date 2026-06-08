47e021d2ad16d29724493d0d3e88b982682194a1
Mahashivkarn <dhruvkumarrajput007@gmail.com>
2026-06-08 14:37:15 +0530

    Using Recursion

--- classwork.c
+++ classwork.c
@@ -1,29 +1,21 @@
 #include<math.h>
 #include<stdio.h>
-float squarearea(float a);
-float circlearea(float b);
-float rectanglearea(float c,float d);
+void printHW(int count);
 
 int main()
- {
-  float a,b,c,d;
-  printf("Enter a,b,c&d");
-  scanf("%f %f %f %f",&a,&b,&c,&d);
-  printf("Area of square:%f\n",squarearea(a));
-  printf("Area of circle:%f\n",circlearea(b));
-  printf("Area of rectangle:%f\n",rectanglearea(c,d));
-  return 0; 
- }
- 
- float squarearea(float a)
- {
-   return a*a;
- }
- float circlearea(float b)
- {
-  return 3.14*b*b;
- }
-float rectanglearea(float c,float d)
 {
-  return c*d;
+  printHW(5); 
+
+  return 0;
+}
+
+void printHW(int count)
+{
+  if(count==0)
+  {
+    return;
+  }
+    printf("Hello World\n");
+    printHW(count-1);
+  
 }
