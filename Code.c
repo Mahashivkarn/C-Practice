5b932b33177d4c2edc542ae4b349005d6ac07a93
Mahashivkarn <dhruvkumarrajput007@gmail.com>
2026-06-09 10:29:49 +0530

    Printing Fibonacci series using For Loop.

--- Code.c
+++ Code.c
@@ -1,16 +1,16 @@
-#include <stdio.h>
+  #include <stdio.h>
 
-float converttemp(float celsius);
-
-int main()
-{  
-   float far = converttemp(0);
-   printf("f:%f",far);
- return 0;
-}
-
-float converttemp(float celsius)
-{
-  float far=celsius*(9.0/5.0) + 32;
-  return far;
-}
+  int main()
+  {
+    int n,sum=0,nm1=1,nm2;
+    printf("Enter a number");
+    scanf("%d",&n);
+    for(int i=1;i<=n;i++)
+    {
+        printf("%d",sum);
+        nm2=nm1+sum;
+        sum=nm1;
+        nm1=nm2;  
+    }
+    return 0;
+  }
