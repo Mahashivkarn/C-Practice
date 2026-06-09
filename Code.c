793e17458796014cf24765a087354cdd5702024a
Mahashivkarn <dhruvkumarrajput007@gmail.com>
2026-06-09 15:31:48 +0530

    Swaping Two numbers.

--- Code.c
+++ Code.c
@@ -1,23 +1,33 @@
   #include <stdio.h>
-
+  
+  void swap(int a,int b);
+  void _swap(int *a, int *b);
   int main()
   {
-    int *ptr;
-    int x;
-
-    ptr =&x;
-    *ptr=0;
-
-    printf("x= %d\n",x);
-    printf("*ptr =%d \n",*ptr);
-
-    *ptr+=5;
-    printf(" x = %d\n",x);
-    printf(" *ptr = %d\n",*ptr);
-
-    (*ptr)++;
-    printf(" x= %d\n", x);
-    printf(" *ptr =%d\n",*ptr);
-    
+    int x =3,y=5;
+    swap(3,5);
+    printf("swap x=%d y=%d\n",x,y);
+    _swap(&x,&y);
+    printf("swap a=%d b=%d\n",x,y);
     return 0;
+  }
+  
+  void swap(int a,int b)
+  {
+    int c;
+    c=a;
+    a=b;
+    b=c;
+    printf("swap a=%d b=%d\n",a,b);
+    return a,b;
+  }
+
+  void _swap(int *a, int *b)
+  {
+    int c;
+     c=*a;
+    *a=*b;
+    *b=c;
+    printf("swap a=%d b=%d\n",*a,*b);
+    return *a,*b;
   }
