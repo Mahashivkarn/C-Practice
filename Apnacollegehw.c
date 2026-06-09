ef23f76e1d70e3fbd318bd456c6c63eecfc0fb3a
Mahashivkarn <dhruvkumarrajput007@gmail.com>
2026-06-09 09:42:40 +0530

    Fibonacci series to print nth term.

--- Apnacollegehw.c
+++ Apnacollegehw.c
@@ -1,9 +1,24 @@
 #include<stdio.h>
+
+int fib(int n);
+
 int main(){
-    for(int i=1;i<=5;i++){
-        for(int j=1;j<=i;j++){
-            printf("*");
-        }
-        printf("\n");
-    }
+    printf("Fib is %d\n", fib(6));
+    return 0;
+}
+
+int fib(int n)
+{
+ if(n==0){
+    return 0;
+ }
+ if(n==1)
+ {
+    return 1;
+ }
+    int fibNm2=fib(n-2);    
+    int fibNm1=fib(n-1);
+    int fibN=fibNm2 +fibNm1;
+    // printf("fib of %d :%d\n",n,fibN);
+    return fibN;
 }
