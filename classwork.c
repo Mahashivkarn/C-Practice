f0e5b44191327ac3db29e2ee9b274c99257b3146
Mahashivkarn <dhruvkumarrajput007@gmail.com>
2026-06-10 18:22:45 +0530

    Printing Marks by Array.

--- classwork.c
+++ classwork.c
@@ -2,11 +2,14 @@
 
 int main()
 {
-  char *ptr;
-
-  for(*ptr='A';*ptr<='Z';(*ptr)++)
-  {
-    printf("%c\n",*ptr);
-  }
+ int marks[3];
+ printf("Enter Maths marks");
+ scanf("%d",&marks[0]);
+ printf("Enter chemistry marks");
+ scanf("%d",&marks[1]);
+ printf("Enter physics marks");
+ scanf("%d",&marks[2]);
+ 
+ printf("Phy =%d\n chem=%d\n math=%d",marks[0],marks[1],marks[2]);
   return 0;
 }
