51ea895f5b5305a7ea1469650621a5f81c542557
Mahashivkarn <dhruvkumarrajput007@gmail.com>
2026-06-10 18:33:20 +0530

    Printing final price of product after gst using array.

--- Apnacollegehw.c
+++ Apnacollegehw.c
@@ -1,17 +1,16 @@
 #include<stdio.h>
 
-void work(int a, int b, int *sum, int *prod, int *avg);
 int main()
 {
-   int a=2,b=4;
-   int sum,prod,avg;
-   work(a,b, &sum,  &prod, &avg);
-   printf("sum =%d,prod=%d,avg=%d",sum,prod,avg);  
-}
+   float price[3];
 
-void work(int a, int b, int *sum, int * prod, int *avg)
-{
-   *sum=a+b;
-   *prod=a*b;
-   *avg=(a+b)/2;
+   printf("Enter price 0f 3 products");
+
+   scanf("%f %f %f",&price[0],&price[1],&price[2]);
+
+   printf("GST price of product are : %f %f %f \n",price[0]*0.18,price[1] *0.18,price[2]*0.18);
+
+   printf("Final price of product are : %f %f %f\n",price[0]*0.18 + price[0],price[1] *0.18 + price[1],price[2]*0.18 + price[2]);
+
+   return 0;
 }
