88c0b3652a512fc12ef21f0c90ad26215051f193
Mahashivkarn <dhruvkumarrajput007@gmail.com>
2026-06-11 10:07:33 +0530

    Using pointer in array.

--- Apnacollegehw.c
+++ Apnacollegehw.c
@@ -2,15 +2,14 @@
 
 int main()
 {
-   float price[3];
+   int age =22;
+   int *ptr=&age;
 
-   printf("Enter price 0f 3 products");
+   printf("%p\n",ptr);
 
-   scanf("%f %f %f",&price[0],&price[1],&price[2]);
+   ptr++;
 
-   printf("GST price of product are : %f %f %f \n",price[0]*0.18,price[1] *0.18,price[2]*0.18);
-
-   printf("Final price of product are : %f %f %f\n",price[0]*0.18 + price[0],price[1] *0.18 + price[1],price[2]*0.18 + price[2]);
+   printf("%p\n",ptr);
 
    return 0;
 }
