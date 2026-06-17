609fc4b73a1a515e50af266c08295ac35d905b25
Mahashivkarn <dhruvkumarrajput007@gmail.com>
2026-06-17 10:50:48 +0530

    Compile working right to left.

--- Code.c
+++ Code.c
@@ -1,23 +1,8 @@
   #include <stdio.h>
   
-  int maxi(int *a,int *b);
-
-  int main()
-  {
-    int a, b;
-    printf("Enter a and b");
-    scanf("%d %d",&a,&b);
-    int max = maxi(&a,&b);
-    printf("From number %d and %d :%d is greater\n",a,b,max);
+   int main()
+   {
+    int a=1;
+    printf("%d %d %d\n",a,++a,a++);
     return 0;
-  }
-
-  int maxi(int *a,int *b)
-  {
-    if(*a>*b){
-      return *a;
-    }
-    if(*b>*a){
-      return *b;
-    }
-  }
+   }  
