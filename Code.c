39df62149deffe03086ad1f7dc52b8959e2ff534
Mahashivkarn <dhruvkumarrajput007@gmail.com>
2026-06-17 16:09:51 +0530

    Printing Leap Year or not by functions.

--- Code.c
+++ Code.c
@@ -1,8 +1,27 @@
   #include <stdio.h>
   
+   int year(int);
+
+
    int main()
    {
-    int a=1;
-    printf("%d %d %d\n",a,++a,a++);
+    int a,n;
+    
+    printf("\nEnter a Year:\n");
+    scanf("%d",&a);
+
+    n=year(a);
+    
     return 0;
    }  
+
+   int year(int x)
+   {
+    if((x%400==0)||(x%100 !=0 && x%4==0))
+    {
+      return printf("Leap Year\n");
+    } else
+    {
+      return printf("not a Leap Year\n");
+    }
+   }
