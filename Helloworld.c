0677551d2ed0a778dd2a776c3c28a4b0b2734840
Mahashivkarn <dhruvkumarrajput007@gmail.com>
2026-06-18 15:37:31 +0530

    Using Sizeof Function

--- Helloworld.c
+++ Helloworld.c
@@ -2,28 +2,10 @@
 
 int main()
 {
-    int i=3,*j,**k;
-    j=&i;
-    k=&j;
+    int *alpha;
+    char *ch;
+    float *s;
 
-    printf("\nAddress Of i=%u\n",&i);
-    printf("Address of i=%u\n",j);
-    printf("Address of i=%u\n",*k);
-
-    printf("\nAddress of j=%u\n",&j);
-    printf("Address of j=%u\n",k);
-
-    printf("\nAddress of k=%u\n",&k);
-
-    printf("\nValue of k=%d\n",k);
-
-    printf("\nValue of j=%d\n",j);
-    printf("Value of j=%d\n",*(&j));
-    printf("Value of j=%d\n",*k);
-
-    printf("\nValue of i=%d\n",i);
-    printf("Value of i=%d\n",*j);
-    printf("Value of i=%d\n",*(&i));
-    printf("Value of i=%d\n",**k);
+    printf("%d %d %d",sizeof(alpha),sizeof(ch),sizeof(s));
     return 0;
 }
