5941edcc51d7c3807f6ce85e852e272914ca36eb
Mahashivkarn <dhruvkumarrajput007@gmail.com>
2026-06-18 15:35:01 +0530

    Pointer of Pointer Integer .

--- Helloworld.c
+++ Helloworld.c
@@ -2,15 +2,28 @@
 
 int main()
 {
-    int i=3;
-    int *j;
+    int i=3,*j,**k;
     j=&i;
-    printf("Address of i =%u\n",&i);
+    k=&j;
+
+    printf("\nAddress Of i=%u\n",&i);
     printf("Address of i=%u\n",j);
-    printf("Address of j=%u\n",&j);
-    printf("Value of j=%u\n",j);
-    printf("Value of i=%d\n",i);
-    printf("Value of i=%d\n",*(&i));
+    printf("Address of i=%u\n",*k);
+
+    printf("\nAddress of j=%u\n",&j);
+    printf("Address of j=%u\n",k);
+
+    printf("\nAddress of k=%u\n",&k);
+
+    printf("\nValue of k=%d\n",k);
+
+    printf("\nValue of j=%d\n",j);
+    printf("Value of j=%d\n",*(&j));
+    printf("Value of j=%d\n",*k);
+
+    printf("\nValue of i=%d\n",i);
     printf("Value of i=%d\n",*j);
+    printf("Value of i=%d\n",*(&i));
+    printf("Value of i=%d\n",**k);
     return 0;
 }
