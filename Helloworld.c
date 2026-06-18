17206284423152ccf9452124263daf955d09c19d
Mahashivkarn <dhruvkumarrajput007@gmail.com>
2026-06-18 15:46:52 +0530

    Call By reference.

--- Helloworld.c
+++ Helloworld.c
@@ -1,20 +1,20 @@
 #include<stdio.h>
 
-void swap(int x, int y);
+void swap(int *, int *);
 
 int main()
 {
     int a=10,b=20;
-    swap(a,b);
+    swap(&a,&b);
     printf("a=%d b=%d\n",a,b);
     return 0;
 }
 
-void swap(int x,int y)
+void swap(int *x,int *y)
 {
   int t;
-  t=x;
-  x=y;
-  y=t;
-  printf("X=%d Y=%d\n",x,y);
+  t=*x;
+  *x=*y;
+  *y=t;
+  printf("X=%d Y=%d\n",*x,*y);
 }
