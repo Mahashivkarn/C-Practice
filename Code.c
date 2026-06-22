47b81b5d23d2c512ebd29fc287a2c5e4c8131e57
Mahashivkarn <dhruvkumarrajput007@gmail.com>
2026-06-22 10:18:47 +0530

    Printing sum average and standard deviation by call by

--- Code.c
+++ Code.c
@@ -1,31 +1,24 @@
 #include<stdio.h>
+#include<math.h>
 
-void pf(int n);
+int fun(int*,float*,double*);
 
 int main()
 {
-  int n;
-  printf("Enter a number:");
-  scanf("%d",&n);
-
-  printf("Factorial Of number are\n");
-  pf(n);
+  int sum;
+  float avg;
+  double std;
+  fun(&sum, &avg,&std);
+  printf("%d %f %lf",sum,avg,std);
   return 0;
 }
 
-void pf(int n)
+int fun(int *x,float *y,double *z)
 {
-   int i =2;
-
-    while(n>1)
-    {
-      if(n%i==0)
-      {
-        printf("%d\n",i);
-        n=n/i;
-              }else
-              {
-                i++;
-              }    
-          }   
-    }
+  int n1,n2,n3,n4,n5;
+  printf("\n Enter 5 number : ");
+  scanf("%d %d %d %d %d",&n1,&n2,&n3,&n4,&n5);
+  *x=n1+n2+n3+n4+n5;
+  *y=(n1+n2+n4+n5)/5;
+  *z=sqrt((pow((n1-*y),2.0))+pow((n2-*y),2.0) +pow((n3-*y),2.0) +pow((n4-*y),2.0)+pow((n5-*y),4));
+}
