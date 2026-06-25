c139bfb6264f47911a063f7b0360a0cbf6bafb1d
Mahashivkarn <dhruvkumarrajput007@gmail.com>
2026-06-25 10:47:59 +0530

    Point Inside Triangle

--- Helloworld.c
+++ Helloworld.c
@@ -1,21 +1,61 @@
 #include<stdio.h>
+#include<math.h>
 
-void shift(float *,float *,float *,float  *);
+int dist(float ,float,float,float);
+int area(float ,float,float,float,float,float);
+int inside(float,float,float,float,float,float,float,float);
 int main()
 {
-  float kg,gm,ton,pound;
-  printf("Enter the amount of Kg to Convert:");
-  scanf("%f",&kg);
+  float x1,y1,x2,y2,x3,y3,x,y;
 
-  shift(&kg,&gm,&ton,&pound);
+  printf("Enter A(x1,y1):");
+  scanf("%f %f",&x1,&y1);
 
-  printf(" in Kg=%f\n in Gram=%f\n in Ton=%f\n in Pound=%f\n",kg,gm,ton,pound);
+  printf("Enter B(x2,y2):");
+  scanf("%f %f",&x2,&y2);
+
+  printf("Enter C(x3,y3):");
+  scanf("%f %f",&x3,&y3);
+
+  printf("Enter D(x,y):");
+  scanf("%f %f",&x,&y);
+
+  if(inside( x1,y1,x2,y2,x3,y3,x,y))
+   printf("Point D is inside the triangle ABC\n");
+  else
+   printf("Point D is outside the triangle ABC\n");
   return 0;
 }
 
-void shift(float *kg,float *a,float *b,float *c)
+int dist(float x1,float y1,float x2,float y2)
 {
-  *a=*kg*1000;
-  *b=*kg/1000;
-  *c=*kg * 2.204;
+ return sqrt((x2-x1)*(x2-x1)+(y2-y1)*(y2-y1));
+}
+int area(float x1,float y1,float x2,float y2,float x3,float y3)
+{
+ float a,b,c,s;
+
+ a=dist(x1,y1,x2,y2);
+ b=dist(x2,y2,x3,y3);
+ c=dist(x3,y3,x1,y1);
+
+ s=(a+b+c)/2;
+
+ return sqrt(s*(s-a)*(s-b)*(s-c));
+}
+
+int inside(float x1,float y1,float x2,float y2,float x3,float y3,float x,float y)
+{
+  float A1,A2,A3,A;
+
+  A=area(x1,y1,x2,y2,x3,y3);
+
+  A1=area(x,y,x1,y1,x2,y2);
+  A2=area(x,y,x2,y2,x3,y3);
+  A3=area(x,y,x3,y3,x1,y1);
+
+  if(fabs(A-(A1+A2+A3)) < 0.0001)
+   return 1;
+
+   return 0;
 }
