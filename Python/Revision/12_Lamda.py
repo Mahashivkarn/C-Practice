4cda46afa103b17c4383a93133bf411149ad8a0a
Mahashivkarn <dhruvkumarrajput007@gmail.com>
2026-07-30 15:10:14 +0530

    Making my module and printiing it.

--- Python/Revision/12_Lamda.py
+++ Python/Revision/12_Lamda.py
@@ -1,6 +1,9 @@
+import mymodule
 square =lambda x: x*x
 #These are used to replace function and wite it in one line 
 print(square(4))
 sum =lambda x,y: x+y
 
 print(sum(4,5))
+mymodule.hello()
+mymodule.avg(2,34,432)
